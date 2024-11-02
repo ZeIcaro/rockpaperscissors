b043ab88399a56599f8bc6bce85b3b2e630f2768
ZeIcaro <89083008+ZeIcaro@users.noreply.github.com>
2024-11-02 18:58:15 -0300

    Write getHumanChoice and PlayRound functions
    Add getHumanChoice function to capture the user
    choice with a prompt, in a case insensitive
    manner. Add PlayRound function which compares
    humanChoice with computerChoice, determining the
    winner and incrementing the respective score by
    one.

--- rps-game.js
+++ rps-game.js
@@ -1,17 +1,54 @@
 console.log("Hello world")
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice () {
     let rockv = 1;
     let paperv = 2;
     let scissorsv = 3;
     choiceNum = Math.random() * 3;
     if (choiceNum <= 1) {
-        return "Rock";
+        return "rock";
     } else if (choiceNum <= 2) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice () {
+    let userChoice = prompt("Rock, paper or scissors?");
+    return userChoice.toLowerCase()
+}
+
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log(`${humanChoice} beats ${computerChoice}. You win!`);
+        ++humanScore;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`${humanChoice} beats ${computerChoice}. You win!`);
+        ++humanScore;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log(`${humanChoice} beats ${computerChoice}. You win!`);
+        ++humanScore;
+    } else if (humanChoice === computerChoice) {
+        return "Draw!"
+    } else {
+        console.log(`${computerChoice} beats ${humanChoice}. You lose!`);
+        ++computerScore;
+    }
+}
+
+/* rock beats scissors; scissors beats paper; paper beats rock */
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(computerScore)
+console.log(humanScore)
+
+
